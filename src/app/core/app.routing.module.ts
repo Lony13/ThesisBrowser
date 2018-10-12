58d70e356f045ccfb77bb1befa4f144b5e96a9ba
Lony13 <wojteklony@gmail.com>
2018-10-12 19:34:13 +0200

    Add sidebar component

--- src/app/core/app.routing.module.ts
+++ src/app/core/app.routing.module.ts
@@ -2,10 +2,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {LoginComponent} from '../login/login.component';
 import {HomeDashboardComponent} from '../home-dashboard/home-dashboard.component';
+import {AdminDashboardComponent} from '../admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeDashboardComponent},
   { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminDashboardComponent},
   {path : '', component : HomeDashboardComponent}
 ];
 
