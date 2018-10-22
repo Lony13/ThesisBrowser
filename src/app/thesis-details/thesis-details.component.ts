d6325d5092f40cf910cec44ef66872dda67b48df
Lony13 <wojteklony@gmail.com>
2018-10-22 18:48:17 +0200

    Add logic for details, mock data

--- src/app/thesis-details/thesis-details.component.ts
+++ src/app/thesis-details/thesis-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {ThesisService} from '../../thesisService/thesis.service';
+import {ThesisService} from '../thesisService/thesis.service';
 import {ThesisDetails} from '../model/thesisDetails';
 import {Observable} from 'rxjs';
 
@@ -17,6 +17,15 @@ export class ThesisDetailsComponent implements OnInit {
   thesisDetailsObservable: Observable<ThesisDetails>;
 
   ngOnInit() {
+    this.thesisDetails = new ThesisDetails();
+    this.thesisDetails.title = 'To jest tytuł';
+    this.thesisDetails.authors = ['Dr Inż Piotr Faliszewski', 'Zbigniew Kaleta'];
+    this.thesisDetails.linkToPDF = 'https://www.google.com';
+    this.thesisDetails.date = new Date(2018, 10, 22);
+    this.thesisDetails.cititationNo = 12;
+    this.thesisDetails.keyWords = ['wojna', 'irak', 'tańce słowaińskie'];
+    this.thesisDetails.relatedTheses = ['jakas praca', 'inna praca', 'taniec powszechny'];
+
     this.thesisDetailsObservable = this.thesisService.getThesisDetailsById(this.thesisService.thesisId);
     this.thesisDetailsObservable.subscribe(value => this.thesisDetails = value);
     console.log(this.thesisDetails);
