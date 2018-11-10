52f79293ff21502abf01bef84dd1cb90a2323374
Lony13 <wojteklony@gmail.com>
2018-11-10 17:55:03 +0100

    Style thesis details

--- src/app/model/thesisDetails.ts
+++ src/app/model/thesisDetails.ts
@@ -1,7 +1,7 @@
 import {Thesis} from './thesis';
 
 export class ThesisDetails extends Thesis {
-  cititationNo: number;
+  citationNo: number;
   publicationDate: Date;
   relatedTheses: string[];
   keyWords: string[];
